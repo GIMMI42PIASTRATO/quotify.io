3acd50ec64bfe42105ee1dff1d0f43ac8cff6e60
GIMMI42PIASTRATO <67071988+GIMMI42PIASTRATO@users.noreply.github.com>
2024-08-25 16:30:40 +0200

    chore: add Navbar component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { inter } from "@/utils/fonts";
 import "./globals.css";
+import Navbar from "@/components/Navbar";
 
 export const metadata: Metadata = {
 	title: "Create Next App",
@@ -15,6 +16,7 @@ export default function RootLayout({
 	return (
 		<html lang="en">
 			<body className={`${inter.className} text-gray-700 antialiased`}>
+				<Navbar />
 				{children}
 			</body>
 		</html>
