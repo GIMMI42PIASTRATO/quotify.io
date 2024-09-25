900ecf10dc3862f836e9289f83c22377c37c0afa
GIMMI42PIASTRATO <67071988+GIMMI42PIASTRATO@users.noreply.github.com>
2024-09-25 09:23:23 +0200

    feat(AsideNavbar): add hover effect to VerticalNavButton and AsideNavbar

--- src/components/AsideNavbar.tsx
+++ src/components/AsideNavbar.tsx
@@ -1,17 +1,36 @@
+"use client";
+
 import VerticalNavButton from "./VerticalNavButton";
 import { LayoutTemplate } from "lucide-react";
 import { Type } from "lucide-react";
+import { useState } from "react";
+import { cn } from "@/lib/utils";
 
 export default function AsideNavbar() {
+	const [isHover, setIsHover] = useState(false);
+
 	return (
 		<aside>
-			<nav className="ml-2 mt-3 w-[72px] text-muted-foreground">
-				<div className="flex flex-col gap-3">
-					<VerticalNavButton htmlFor="template" label="Template">
+			<nav
+				className={cn(
+					"pl-4 pr-4 pt-6 w-24 text-muted-foreground",
+					isHover ? "bg-muted" : ""
+				)}
+			>
+				<div className="flex flex-col">
+					<VerticalNavButton
+						htmlFor="template"
+						label="Template"
+						setIsHover={setIsHover}
+					>
 						<LayoutTemplate size={24} />
 					</VerticalNavButton>
 
-					<VerticalNavButton htmlFor="text" label="Text">
+					<VerticalNavButton
+						htmlFor="text"
+						label="Text"
+						setIsHover={setIsHover}
+					>
 						<Type size={24} />
 					</VerticalNavButton>
 				</div>
--- src/components/VerticalNavButton.tsx
+++ src/components/VerticalNavButton.tsx
@@ -6,6 +6,7 @@ type VerticalNavButtonProps = {
 	className?: string;
 	htmlFor: string;
 	label: string;
+	setIsHover: (value: boolean) => void;
 };
 
 export default function VerticalNavButton({
@@ -13,13 +14,16 @@ export default function VerticalNavButton({
 	className,
 	htmlFor,
 	label,
+	setIsHover,
 }: VerticalNavButtonProps) {
 	return (
 		<div
 			className={cn(
-				"flex items-center justify-center flex-col h-[72px] w-full gap-1 cursor-pointer group",
+				"flex items-center justify-center flex-col h-[90px] w-full gap-1 cursor-pointer group",
 				className
 			)}
+			onMouseEnter={() => setIsHover(true)}
+			onMouseLeave={() => setIsHover(false)}
 		>
 			<Button
 				className="group-hover:shadow-xl group-hover:text-yellow-400 group-hover:drop-shadow-xl group-hover:bg-gray-100"
